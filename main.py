f5e3af50630c15745653221d9223854a104c94b4
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2012-12-25 17:15:52 -0400

    Added script to populate the mongodb. Added some templates that use Twitter Bootstrap and tested mongodb.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
+
 from flask import Flask, jsonify, render_template, request
 from flask.ext.pymongo import PyMongo
+from flask.ext.bootstrap import Bootstrap
+
 import json_app
 
 app = json_app.make_json_app('__main__')
 mongo = PyMongo(app)
+Bootstrap(app)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/item')
+def get_item():
+    look = mongo.db.looks.find_one()
+    return render_template('look.html', title = look["title"])
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ populatedb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from pymongo import MongoClient
+import datetime
+
+connection = MongoClient()
+db = connection.main
+looks = db.looks
+
+item1 = {"name":"Clark's Dessert Boots",
+        "price":110.00,
+        "type":"boot",
+        "screenshot":"blah.jpg",
+        "link":"http://google.com/"}
+
+item0 = {"name":"Levi's 511",
+        "price":37.00,
+        "type":"denim",
+        "screenshot":"blah.jpg",
+        "link":"http://google.com/"}
+
+items = [item0, item1]
+
+look = {"title": "Mike",
+        "text": "My first look!",
+        "tags": ["casual", "hawt", "reddit"],
+        "items": items}
+
+looks.insert(look)
