30f92ecf08c2333e13229c49d5a3e89c2e6046b3
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2012-12-25 22:31:52 -0400

    Looks are now shown in the main page

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
+# -*- coding: utf8 -*- 
 
 # Copyright (C) 2012 Gabriel J. Pérez Irizarry and Andrea Del Risco
-
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License, version 3,
 # as published by the Free Software Foundation.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details.
-
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
@@ -67,7 +68,9 @@ def add_look(form):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    looks = mongo.db.looks.find().limit(10)
+        
+    return render_template('index.html', looks = looks)
 
 @app.route('/submit_new_look/', methods=('GET', 'POST'))
 def submit_new_look():
